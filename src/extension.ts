e2fcb162ecbb0b3b181e376378ab9bfdf899d094
Norman Reed <norman@normanreed.com>
2020-05-29 22:03:33 -0400

    add ability to run single actions add hints to actions configuration add editor.action.fixAll to default actions remove command from editor title context menu improve progress granularity to include actions for each file package.json: fix test script pointing at wrong file package.json: add prepack script

--- src/extension.ts
+++ src/extension.ts
@@ -1,60 +1,75 @@
 'use strict';
 
 import * as fs from 'fs';
+import * as path from 'path';
 import * as vscode from 'vscode';
 
-interface ProgressConfig {
+const time = () => new Date().getTime();
+const elapsed = (start: number) => time() - start;
+
+interface ProgressUpdate {
     message?: string;
     increment?: number;
 }
 
-const FROM_SCM_CONTEXT = 'extension.applyFileActionsFromScmContext';
-const FROM_EDITOR_TILE_CONTEXT = 'extension.applyFileActionsEditorTile';
-const FROM_EXPLORER_CONTEXT = 'extension.applyFileActionsFromExplorerContext';
+const CHOOSE_ACTION = 'extension.chooseAction';
+const RUN_ACTIONS = 'extension.runActions';
 
+const getEnabledActions = () => vscode.workspace.getConfiguration().get('contextMenuActions.actions') as Array<string>;
 
-const applyActions = async (uris: vscode.Uri[]) => {
-    // Getting current settings
-    const actions = vscode.workspace.getConfiguration().get('contextMenuActions.actions') as Array<string>;
+const applyActions = async (uris: vscode.Uri[], actions = getEnabledActions()) => {
+    const allFileActions = uris.reduce((acts: any[], uri) => {
+        const a = actions.reduce((facts: any[], action: string) => {
+            return [...facts, { uri, action }];
+        }, []);
+        return [...acts, ...a];
+    }, []);
+    const completedFileActions = [];
+    const errors = [];
     const saveAfterFormat = vscode.workspace.getConfiguration().get('contextMenuActions.save') as boolean;
     const closeAfterSave = vscode.workspace.getConfiguration().get('contextMenuActions.close') as boolean;
-
-    const increment = (1 / uris.length) * 100;
+    const increment = (1 / allFileActions.length) * 100;
+    const started = time();
 
     const progressOptions: vscode.ProgressOptions = {
         location: vscode.ProgressLocation.Notification,
-        title: 'Applying actions to files',
+        title: `Running `,
         cancellable: true,
     };
 
-    vscode.window.withProgress(progressOptions, async (progress: vscode.Progress<ProgressConfig>, cancellationToken: vscode.CancellationToken) => {
-        for (let i = 0; i < uris.length; i++) {
-            const uri = uris[i];
-            if (cancellationToken.isCancellationRequested) {
-                break;
-            }
+    await vscode.window.withProgress(progressOptions, async (progress: vscode.Progress<ProgressUpdate>, cancellationToken: vscode.CancellationToken) => {
+        for (let i = 0; i < allFileActions.length && !cancellationToken.isCancellationRequested; i++) {
+            const fact = allFileActions[i] as { uri: vscode.Uri, action: string };
+            const filename = path.basename(fact.uri.fsPath);
+
             try {
-                progress.report({
-                    message: `${i + 1}/${uris.length}`
-                });
-                await vscode.window.showTextDocument(uris[i], { preserveFocus: false, preview: true });
-                actions.forEach(async a => await vscode.commands.executeCommand(a, uri));
-                // await vscode.commands.executeCommand('editor.action.formatDocument', uri);
+                await vscode.window.showTextDocument(fact.uri, { preserveFocus: false, preview: true });
+                await vscode.commands.executeCommand(fact.action, fact.uri);
                 if (saveAfterFormat) {
-                    await vscode.commands.executeCommand('workbench.action.files.save', uri);
+                    await vscode.commands.executeCommand('workbench.action.files.save');
                     if (closeAfterSave) {
-                        await vscode.commands.executeCommand('workbench.action.closeActiveEditor', uri);
+                        await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
                     }
                 }
-            } catch (exception) {
-                vscode.window.showWarningMessage(`Could not format file ${uri}`);
+                completedFileActions.push(fact);
+            } catch (e) {
+                errors.push({ ...fact, error: e });
+                vscode.window.showErrorMessage('Error running %s on %s: %s', fact.action, fact.uri.fsPath, e);
+                console.error(e);
             }
+
             progress.report({
                 increment: increment,
+                message: `${i + 1}/${allFileActions.length} - ${filename}`
             });
         }
-    });
 
+        vscode.window.showInformationMessage(`
+        Completed ${completedFileActions.length} actions 
+        in ${elapsed(started)}ms
+        ${errors.length > 0 ? errors.length + ' errors encountered' : ''}
+        `);
+    });
 };
 
 const getRecursiveUris = async (uris: vscode.Uri[]) => {
@@ -74,25 +89,44 @@ const getRecursiveUris = async (uris: vscode.Uri[]) => {
     return outputUris;
 };
 
+function resolveFiles(...args: any[]) {
+    if (args.length > 0) {
+        if (args[0] instanceof vscode.Uri) {
+            const clicked = args[0] as vscode.Uri;
+            if (args.length === 2) {
+                const selected = args[1] as vscode.Uri[];
+                return selected;
+            }
+            return [clicked];
+        } else {
+            const selected = args as vscode.SourceControlResourceState[];
+            return selected.map(x => x.resourceUri);
+        }
+    }
+    console.warn('No files selected for', args);
+    return [];
+}
+
+async function promptAction() {
+    return await vscode.window.showQuickPick(
+        getEnabledActions()
+    );
+}
+
 export function activate(context: vscode.ExtensionContext) {
-
-
     context.subscriptions.push(...[
-
-        vscode.commands.registerCommand(FROM_SCM_CONTEXT, async (...selectedFiles: vscode.SourceControlResourceState[]) => {
-            const uris = await getRecursiveUris(selectedFiles.map(x => x.resourceUri));
-            await applyActions(uris);
+        vscode.commands.registerCommand(RUN_ACTIONS, async (...args: any[]) => {
+            const f = await getRecursiveUris(resolveFiles(...args));
+            await applyActions(f);
         }),
 
-        vscode.commands.registerCommand(FROM_EDITOR_TILE_CONTEXT, async (clickedFile: vscode.Uri) => {
-            await applyActions([clickedFile]);
-        }),
-
-        vscode.commands.registerCommand(FROM_EXPLORER_CONTEXT, async (clickedFile: vscode.Uri, selectedFiles: vscode.Uri[]) => {
-            const uris = await getRecursiveUris(selectedFiles || [clickedFile]);
-            await applyActions(uris);
+        vscode.commands.registerCommand(CHOOSE_ACTION, async (...args: any[]) => {
+            const f = await getRecursiveUris(resolveFiles(...args));
+            const action = await promptAction();
+            if (action) {
+                applyActions(f, [action]);
+            }
         })
-
     ]);
 
 }
